e188c13ca65e4e69b60e87fd03814fae057e453d
francovaro <francovaro@gmail.com>
2024-04-29 15:08:48 +0200

    support for printf

--- app/peripheral/peripheral.c
+++ app/peripheral/peripheral.c
@@ -17,6 +17,13 @@
 #include "peripheral.h"
 #include "board.h"
 #include "se_services_port.h"
+#include <stdio.h>
+#include "uart_tracelib.h"
+
+static void clock_init(void);
+static void uart_callback(uint32_t event);
+
+extern void clk_init(void); // retarget.c
 
 /**
  * @brief 
@@ -28,4 +35,34 @@ void peripheral_init(void)
 
     /* Initialize the SE services */
     se_services_port_init();
+
+    tracelib_init(NULL, uart_callback);
+
+    clock_init();
+}
+
+/**
+ * @brief 
+ * 
+ */
+static void clock_init(void)
+{
+    uint32_t service_error_code = 0;
+    /* Enable Clocks */
+    uint32_t error_code = SERVICES_clocks_enable_clock(se_services_s_handle, CLKEN_CLK_100M, true, &service_error_code);
+    if(error_code || service_error_code){
+        //printf("SE: 100MHz clock enable error_code=%u se_error_code=%u\n", error_code, service_error_code);
+        return;
+    }
+
+    error_code = SERVICES_clocks_enable_clock(se_services_s_handle, CLKEN_HFOSC, true, &service_error_code);
+    if(error_code || service_error_code){
+        //printf("SE: HFOSC enable error_code=%u se_error_code=%u\n", error_code, service_error_code);
+        return;
+    }
+}
+
+static void uart_callback(uint32_t event)
+{
+    //
 }
